d8b8a524327ab18d95c64d397d43947ba25387db
James Soko <s0k0j4m3s@gmail.com>
2025-05-09 10:38:21 +0200

    Delete transposition.h

--- transposition.h
+++
@@ -1,9 +0,0 @@
-#ifndef TRANSPOSITION_H
-#define TRANSPOSITION_H
-
-#include <string>
-
-std::string transposition_encrypt(const std::string& message, int key);
-std::string transposition_decrypt(const std::string& message, int key);
-
-#endif // TRANSPOSITION_H
