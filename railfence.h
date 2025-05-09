384a7a58352c1072b23c1e24f29c2647c0d7d66a
James Soko <s0k0j4m3s@gmail.com>
2025-05-09 10:37:23 +0200

    Delete railfence.h

--- railfence.h
+++
@@ -1,9 +0,0 @@
-#ifndef RAILFENCE_H
-#define RAILFENCE_H
-
-#include <string>
-
-std::string rail_fence_encrypt(const std::string &message, int key);
-std::string rail_fence_decrypt(const std::string &message, int key);
-
-#endif // RAILFENCE_H
